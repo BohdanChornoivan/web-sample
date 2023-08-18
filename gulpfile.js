7faa64b63710c98cb8406a411c95cd493b1ee434
Bohdan Chornoivan <bohdan.chornoivan@gmail.com>
2023-08-18 18:36:26 +0300

    start new pj

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+
+const browserSync = require('browser-sync');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sassToCSS', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass({
+            errorLogToConsole: true
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 15 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: 'public'
+    });
+    browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('watchFiles', function () {
+    gulp.watch('app/scss/*.scss', gulp.series('sassToCSS'));
+});
+
+gulp.task('default', gulp.parallel('watchFiles', 'serve'));
